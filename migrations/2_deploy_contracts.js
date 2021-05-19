14ab4db6b05d9b20af026f0cb4d2488bc51ea609
Nikolay Butnik <65631838+mochi-dot-py@users.noreply.github.com>
2021-05-18 21:51:41 -0400

    fix: fix an error with TokenFarm deployment script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-undef
-const TokenFarm = artifacts.require('Migrations')
+const TokenFarm = artifacts.require('TokenFarm')
 
 module.exports = function(deployer) {
   deployer.deploy(TokenFarm)
