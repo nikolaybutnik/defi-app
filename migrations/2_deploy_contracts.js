14308074d88b2436b4702d109b0b3227e9e22786
Nikolay Butnik <65631838+mochi-dot-py@users.noreply.github.com>
2021-05-19 21:54:31 -0400

    feat: create a deploy script which deploys all three contracts to the blockchain

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,28 @@
-// eslint-disable-next-line no-undef
+/* eslint-disable no-undef */
 const TokenFarm = artifacts.require('TokenFarm')
+const DappToken = artifacts.require('DappToken')
+const DaiToken = artifacts.require('DaiToken')
 
-module.exports = function(deployer) {
-  deployer.deploy(TokenFarm)
+module.exports = async function(deployer, network, accounts) {
+  // this process puts all the smart contracts on the network
+
+  // deploy Mock Dai Token
+  await deployer.deploy(DaiToken)
+  const daiToken = await DaiToken.deployed()
+
+  // deploy Dapp Token
+  await deployer.deploy(DappToken)
+  const dappToken = await DappToken.deployed()
+
+  // deploy TokenFarm
+  await deployer.deploy(TokenFarm, daiToken.address, dappToken.address)
+  const tokenFarm = await TokenFarm.deployed()
+
+  // transfer all tokens to TokenFarm (1 million)
+  // solidity doesn't handle decimals. The number looks larger than 1 mil because Dapp Token has 18 decimal places.
+  // therefore 1 million is: 1,000,000.000,000,000,000,000,000
+  await dappToken.transfer(tokenFarm.address, '1000000000000000000000000')
+
+  // transer 100 Mock Dai Tokens to investor
+  await daiToken.transfer(accounts[1], '100000000000000000000')
 }
--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -2,6 +2,19 @@
 
 pragma solidity >=0.7.6;
 
+import "./DappToken.sol";
+import "./DaiToken.sol";
+
 contract TokenFarm {
+    // assign state variables
     string public name = "Dapp Token Farm";
+    DappToken public dappToken;
+    DaiToken public daiToken;
+
+    constructor(DappToken _dappToken, DaiToken _daiToken) {
+        // above: type, variable name. The type is the smart contract itself.
+        // this function will only run once when the contrct is deployed to the blockchain
+        dappToken = _dappToken;
+        daiToken = _daiToken;
+    }
 }
