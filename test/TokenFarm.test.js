9e33df0aa927365a79b3635c91150b435a5f7cea
Nikolay Butnik <65631838+mochi-dot-py@users.noreply.github.com>
2021-05-19 22:43:05 -0400

    feat: write a test for Dapp Token name

--- test/TokenFarm.test.js
+++ test/TokenFarm.test.js
@@ -9,13 +9,39 @@ const TokenFarm = artifacts.require('TokenFarm')
 const DappToken = artifacts.require('DappToken')
 const DaiToken = artifacts.require('DaiToken')
 
-contract(TokenFarm, (accounts) => {
-  // write tests here
+function tokens(n) {
+  return web3.utils.toWei(n, 'ether')
+}
+
+// destructure the accounts from the accounts array for clarity
+// owner is the one who deployed the contract to the network, and investor is the buyer of the token
+contract(TokenFarm, ([owner, investor]) => {
+  let daiToken, dappToken, tokenFarm
+
+  before(async () => {
+    // load contracts
+    daiToken = await DaiToken.new()
+    dappToken = await DappToken.new()
+    tokenFarm = await TokenFarm.new(dappToken.address, daiToken.address)
+
+    // transfer all Dapp tokens to farm (1 million)
+    await dappToken.transfer(tokenFarm.address, tokens('1000000'))
+
+    // send tokens to investor
+    await daiToken.transfer(investor, tokens('100'), { from: owner })
+  })
+
   describe('Mock Dai deployment', async () => {
     it('has a name', async () => {
-      const daiToken = await DaiToken.new()
       const name = await daiToken.name()
       assert.equal(name, 'Mock DAI Token')
+    })
+  })
+
+  describe('Dapp Token deployment', async () => {
+    it('has a name', async () => {
+      const name = await dappToken.name()
+      assert.equal(name, 'DApp Token')
     })
   })
 })
