f7460af3f6348eaa7b3f2471536261769ef64d30
Nikolay Butnik <65631838+mochi-dot-py@users.noreply.github.com>
2021-05-21 22:45:54 -0400

    feat: write tests to make sure tokens are unstaked correctly

--- test/TokenFarm.test.js
+++ test/TokenFarm.test.js
@@ -118,6 +118,38 @@ contract(TokenFarm, ([owner, investor]) => {
 
       // ensure that only owner can issue tokens
       await tokenFarm.issueTokens({ from: investor }).should.be.rejected
+
+      // unstake tokens
+      await tokenFarm.unstakeTokens({ from: investor })
+
+      // check results after staking
+      result = await daiToken.balanceOf(investor)
+      assert.equal(
+        result.toString(),
+        tokens('100'),
+        'investor mDai wallet balance correct after staking'
+      )
+
+      result = await daiToken.balanceOf(tokenFarm.address)
+      assert.equal(
+        result.toString(),
+        tokens('0'),
+        'Token Farm mDai balance correct after staking'
+      )
+
+      result = await tokenFarm.stakingBalance(tokenFarm.address)
+      assert.equal(
+        result.toString(),
+        tokens('0'),
+        'investor staking balance correct after staking'
+      )
+
+      result = await tokenFarm.isStaking(investor)
+      assert.equal(
+        result.toString(),
+        'false',
+        'investor staking status correct after staking'
+      )
     })
   })
 })
