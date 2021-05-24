ff3bea584da969d65056eb1c2e2b2b73bb0afdd5
Nikolay Butnik <65631838+mochi-dot-py@users.noreply.github.com>
2021-05-23 23:14:13 -0400

    feat: handle data visibility via loading variable

--- src/components/App.js
+++ src/components/App.js
@@ -62,6 +62,8 @@ class App extends Component {
     } else {
       window.alert('TokenFarm contract not deployed to detected network')
     }
+
+    this.setState({ loading: false })
   }
 
   async loadWeb3() {
